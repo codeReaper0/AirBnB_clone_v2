b32f2724bee609a93b44f916af0a7fe3561c1ed3
codeReaper <boluwatifetella@gmail.com>
2023-10-19 06:58:55 +0100

    changed flask app route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,15 +2,14 @@
 """
 This is module 0-hello_route.py
 
-This module starts a web aplication via Flask
+This module starts a web application via Flask
 """
 from flask import Flask
 app = Flask(__name__)
 
-
-@app.route("/")
+@app.route("/airbnb-onepage/")
 def hello_world():
-        return ("Hello HBNB!")
+    return "Hello HBNB!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port=5000)
